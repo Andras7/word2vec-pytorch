d3e66dc0fbaf9c86f234a4e3a6c086d39a179e0e
marta-sd <m.m.stepniewska@gmail.com>
2019-11-30 11:17:11 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='word2vec',
+    version='0.0.1',
+    description='Skip-gram PyTorch implementation',
+    packages=find_packages(include=['word2vec']),
+    install_requires=[],
+)
+
